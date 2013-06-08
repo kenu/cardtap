c312143e39b7e4ec07a3bd2935b61744856b8508
kenu <kenu.heo@gmail.com>
2013-06-09 05:19:43 +0900

    project to utf-8

--- src/kr/hack/cardtap/CardShareActivity.java
+++ src/kr/hack/cardtap/CardShareActivity.java
@@ -2,12 +2,25 @@ package kr.hack.cardtap;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CardShareActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.cardshare);
+
+		Button shareButton = (Button) findViewById(R.id.share_button);
+
+		shareButton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// 메일 보내고,
+				// 발송 결과 표시
+			}
+		});
 	}
 
 }
